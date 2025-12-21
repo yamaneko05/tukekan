c5766182632e82e2eea747f22dc588554721a28d
yamaneko05 <o.yamaneko0331@gmail.com>
2025-12-22 00:32:37 +0900

    add: prismaのセットアップとマイグレーション作成

---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+import { loadEnvConfig } from "@next/env";
+import { defineConfig } from "prisma/config";
+
+loadEnvConfig(process.cwd());
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"],
+  },
+});
