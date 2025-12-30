bbb098e260ed4ca230eeb1edea0e7f0f2ff3dacc
yamaneko05 <o.yamaneko0331@gmail.com>
2025-12-30 16:24:00 +0900

    add: ロゴとアイコンを追加

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -4,12 +4,18 @@ import Link from "next/link";
 import { Settings } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "./theme-toggle";
+import Image from "next/image";
 
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="mx-auto flex h-14 max-w-2xl items-center justify-between px-4">
-        <h1 className="text-2xl font-logo">ツケカン</h1>
+        <Link href={"/"} className="flex items-center">
+          <Image src={"/icon-192.png"} alt="icon" width={32} height={32} />
+          <h1 className="text-2xl font-logo text-primary tracking-tight">
+            <span className="text-[#e07326]">ツケ</span>カン
+          </h1>
+        </Link>
         <div className="flex gap-2">
           <ThemeToggle />
           <Button variant="ghost" size="icon" asChild>
